919d06d2b681f044d17ccc3b2f9553434aea09fd
Vansh Mathur <vanshmathur00@gmail.com>
2023-05-01 01:23:06 +0530

    adding blockchain code

--- eventScript.js
+++ eventScript.js
@@ -1,34 +1,177 @@
+var eventArray = sessionStorage.getItem("eventArray");
+var organiser = sessionStorage.getItem("organiser");
 
-
-var eventArray = sessionStorage.getItem('eventArray')
+organiser = JSON.parse(organiser);
 eventArray = JSON.parse(eventArray);
-console.log(eventArray)
+console.log(organiser);
+console.log(eventArray);
+const web3 = new Web3("http://localhost:7545");
 
-
-
-
-
-var eventNameField = document.getElementById('eventName');
-var eventDateField = document.getElementById('eventDate');
-var priceField = document.getElementById('price');
+var eventNameField = document.getElementById("eventName");
+var eventDateField = document.getElementById("eventDate");
+var priceField = document.getElementById("price");
+var addressField = document.getElementById("address");
 
 eventNameField.value = eventArray[0].name;
 eventDateField.value = eventArray[0].date;
 priceField.value = eventArray[0].price;
 
-
-
-var countTickets = document.getElementById('countTickets');
-var totalPrice = document.getElementById('totalPrice');
-
+var countTickets = document.getElementById("countTickets");
+var totalPrice = document.getElementById("totalPrice");
 
 const getBill = () => {
-    totalPrice.value = countTickets.value * eventArray[0].price;
-}
+  totalPrice.value = countTickets.value * eventArray[0].price;
+};
 
-
-const payNow = () => {
-    alert('Thanks for booking!!')
-    countTickets.value='';
-    totalPrice.value=''; 
-}
+const payNow = async () => {
+    
+    if (window.ethereum !== "undefined") {
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        account = addressField.value;
+    }
+  const ABI = [
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "id",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "quantity",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "amt",
+          type: "uint256",
+        },
+      ],
+      name: "buyTicket",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "date",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "price",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "ticketCount",
+          type: "uint256",
+        },
+      ],
+      name: "createEvent",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "events",
+      outputs: [
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "date",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "price",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "ticketCount",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "ticketRemain",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "nextId",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "tickets",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+  const Address = "0x31400962eba5545ec99cf6981A9271Fbda5a0805";
+  window.web3 = await new Web3(window.ethereum);
+  window.contract = await new window.web3.eth.Contract(ABI, Address);
+  console.log(account);
+  await window.contract.methods
+    .buyTicket(eventArray[0].id, countTickets.value, totalPrice.value)
+    .send({ from: addressField.value });
+  console.log(addressField.value, "me me eme");
+  await web3.eth.sendTransaction({
+    from: addressField.value,
+    to: organiser,
+    value: web3.utils.toWei(totalPrice.value, "ether"),
+  });
+  alert("Thanks for booking!!");
+  countTickets.value = "";
+  totalPrice.value = "";
+};
--- script.js
+++ script.js
@@ -1,35 +1,189 @@
-console.log('Connected')
+let account;
 
-let eventDetails = []
-var eventNameField = document.getElementById('eventName');
-var eventDateField = document.getElementById('eventDate');
-var numTicketsField = document.getElementById('numTickets');
-var priceField = document.getElementById('price');
+console.log("Connected");
 
-const getVal = () => {
+let eventDetails = [];
+var eventNameField = document.getElementById("eventName");
+var eventDateField = document.getElementById("eventDate");
+var numTicketsField = document.getElementById("numTickets");
+var priceField = document.getElementById("price");
+let organiserAccount = "null";
 
-    console.log(eventNameField.value);
-    console.log(eventDateField.value);
-    console.log(numTicketsField.value);
-    console.log(priceField.value);  
-    
-    var event = {
-        name : eventNameField.value,
-        date : eventDateField.value,
-        tickets : numTicketsField.value,
-        price : priceField.value
-    }
+var id = 0;
 
-    eventDetails.push(event)
+const getVal = async () => {
+  console.log(eventNameField.value);
+  console.log(eventDateField.value);
+  console.log(numTicketsField.value);
+  console.log(priceField.value);
 
-    sessionStorage.setItem('eventArray', JSON.stringify(eventDetails));
+  var event = {
+    id: id,
+    name: eventNameField.value,
+    date: eventDateField.value,
+    tickets: numTicketsField.value,
+    price: priceField.value,
+  };
 
+  eventDetails.push(event);
+  sessionStorage.setItem("eventArray", JSON.stringify(eventDetails));
 
-    eventNameField.value='';
-    eventDateField.value='';
-    numTicketsField.value='';
-    priceField.value=''; 
-}
+  if (window.ethereum !== "undefined") {
+    console.log("its meeeeeeeeeeeeeeeeeee");
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    account = accounts[0];
+    organiserAccount = account;
+    console.log("organiser ", organiserAccount);
+  }
 
-console.log('Events : - ' , eventDetails)
+  sessionStorage.setItem("organiser", JSON.stringify(organiserAccount));
 
+  const ABI = [
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "id",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "quantity",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "amt",
+          type: "uint256",
+        },
+      ],
+      name: "buyTicket",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "date",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "price",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "ticketCount",
+          type: "uint256",
+        },
+      ],
+      name: "createEvent",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "events",
+      outputs: [
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "date",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "price",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "ticketCount",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "ticketRemain",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "nextId",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "tickets",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+  ];
+  const Address = "0x31400962eba5545ec99cf6981A9271Fbda5a0805";
+  window.web3 = await new Web3(window.ethereum);
+  window.contract = await new window.web3.eth.Contract(ABI, Address);
+  console.log(account);
+  await window.contract.methods
+    .createEvent(
+      event.name,
+      event.date,
+      web3.utils.toWei(event.price, "ether"),
+      event.tickets
+    )
+    .send({ from: account });
+  eventNameField.value = "";
+  eventDateField.value = "";
+  numTicketsField.value = "";
+  priceField.value = "";
+  id = id + 1;
+};
+
+console.log("Events : - ", eventDetails);
